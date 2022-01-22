6fb8b90b9ec708f4cff0a1e2a1da141c7b6cc476
Cenut Team <45029967+cenut@users.noreply.github.com>
2022-01-22 12:21:47 +0700

    Update cryptonight_fast.h

--- stratum/algos/cryptonight_fast.h
+++ stratum/algos/cryptonight_fast.h
@@ -7,7 +7,7 @@ extern "C" {
 
 #include <stdint.h>
 
-void cryptonightfast_hash(const char* input, char* output, uint32_t len, int variant);
+void cryptonightfast_hash(const char* input, char* output, uint32_t len);
 void cryptonightfast_fast_hash(const char* input, char* output, uint32_t len);
 
 #ifdef __cplusplus
